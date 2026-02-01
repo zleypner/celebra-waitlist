801514c8df4ae95797ce50503cd4d6b769fc7d75
mpsdev <mparedes.digital@gmail.com>
2026-01-31 23:22:15 -0300

    fix: set outputFileTracingRoot to remove lockfile warning

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
